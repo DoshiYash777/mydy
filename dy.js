83d6816d5e5cf2cf660f93b5b13713d464204059
DoshiYash777 <79749001+DoshiYash777@users.noreply.github.com>
2021-02-27 17:15:21 +0530

    Add files via upload

---
+++ dy.js
@@ -0,0 +1,14 @@
+
+function fun()
+{
+var name=document.getElementById("name").value;
+
+
+  console.log("HELLO BRO!!");
+  //let name=prompt('Name man??');
+  console.log(name);
+  //alert('Thanks BRo');
+  
+  document.getElementById("output").innerHTML='Hello '+name;
+  
+}
